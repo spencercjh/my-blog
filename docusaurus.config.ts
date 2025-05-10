e41f822a855e50ac0556ce8a831e291e26992130
spencercjh <spencercjh@gmail.com>
2025-05-10 12:25:26 +0800

    feat: add table of contents configuration to docusaurus
    Signed-off-by: spencercjh <spencercjh@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -79,6 +79,10 @@ const config: Config = {
         },
       ],
     },
+    tableOfContents: {
+      minHeadingLevel: 2,
+      maxHeadingLevel: 5,
+    },
     footer: {
       style: "dark",
       links: [
